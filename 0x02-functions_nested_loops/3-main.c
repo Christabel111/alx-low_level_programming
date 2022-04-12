7ccbac6b3c0246a9627748a6cea9e6fdc34f8491
Christabel <israelchristabel5@gmail.com>
2022-04-12 10:19:18 -0700

    islower

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int r;
 
-	r = _islowwer('H');
+	r = _islower('H');
 	_putchar(r + '0');
-	r = _islowwer('o');
+	r = _islower('o');
 	_putchar(r + '0');
-	r = _islowwer(108);
+	r = _islower(108);
 	_putchar(r + '0');
 	_putchar('\n');
 	return (0);
