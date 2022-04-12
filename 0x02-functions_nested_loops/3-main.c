02f04e88aee842ac39b05c8380ec34cac8b8e9eb
Christabel <israelchristabel5@gmail.com>
2022-04-12 10:17:10 -0700

    corect

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Check the code
+ *
+ * Return : Always 0
+ */
+
+int main(void)
+{
+	int r;
+
+	r = _islowwer('H');
+	_putchar(r + '0');
+	r = _islowwer('o');
+	_putchar(r + '0');
+	r = _islowwer(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
