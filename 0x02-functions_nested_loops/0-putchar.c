3589e907a4a9c2203b2dc7be7d9e2d537cdf8098
Christabel <israelchristabel5@gmail.com>
2022-04-12 07:37:51 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "alx.h"
+
+/**
+ * main - Entry point
+ * Return : 0
+ */
+
+int main(void)
+{
+	char c[9] = "_putchar";
+	int i = 0;
+
+	while (i < 9)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
