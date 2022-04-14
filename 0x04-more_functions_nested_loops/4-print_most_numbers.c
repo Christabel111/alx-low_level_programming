b22c88cf12334f502bad91488e759685b99283fd
Christabel <israelchristabel5@gmail.com>
2022-04-14 04:13:54 -0700

    four

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,14 +7,16 @@
 
 void print_most_numbers(void)
 {
-	int n;
+	char n = 0;
 
-	n = 0;
 	while (n <= '9')
 	{
 		if (n != '2' && n != '4')
-			_putchar(n);
-		++n;
+		{
+			_putchar('0' + n);
+		}
+
+		n++;
 	}
 	_putchar('\n');
 }
