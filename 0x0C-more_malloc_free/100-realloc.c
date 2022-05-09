520012acd5ad8016c6c1e923dfea827144f4a758
Christabel <israelchristabel5@gmail.com>
2022-05-09 19:38:43 +0100

    malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,43 +13,25 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *mem;
-	char *ptr_copy, *filler;
-	unsigned int index;
+	char *clone, *relloc;
+	unsigned int i;
 
+	if (ptr != NULL)
+		clone = ptr;
+	else
+		return (malloc(new_size));
 	if (new_size == old_size)
 		return (ptr);
-
-	if (ptr == NULL)
-	{
-		mem = malloc(new_size);
-		
-		if (mem == NULL)
-			return (NULL);
-
-		return (mem);
-	}
-
 	if (new_size == 0 && ptr != NULL)
-	{
 		free(ptr);
-		return (NULL);
-	}
-
-	ptr_copy = ptr;
-	mem = malloc(sizeof(*ptr_copy) * new_size);
-
-	if (mem == NULL)
+	return (0);
+	relloc = malloc(new_size);
+	if (relloc == NULL)
+		return (0);
+	for (i = 0; i < (old_size || i < new_size); i++)
 	{
-		free(ptr);
-		return (NULL);
+		*(relloc + i) = clone[i];
 	}
-
-	filler = mem;
-
-	for (index = 0; index < old_size && index < new_size; index++)
-		filler[index] = *ptr_copy++;
-
 	free(ptr);
-	return (mem);
+	return (relloc);
 }
