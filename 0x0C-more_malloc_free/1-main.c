3907fe93c4ab76815b42d0b3d216c736585a5378
Christabel <israelchristabel5@gmail.com>
2022-05-10 16:18:02 -0700

    m an d f

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -12,8 +12,8 @@ int main(void)
 {
 	char *concat;
 
-	 concat = string_nconcat("Best ", "School !!!", 6);
-	 printf("%s\n", concat);
-	 free(concat);
-	 return (0);
+	concat = string_nconcat("Best ", "School !!!", 6);
+	printf("%s\n", concat);
+	free(concat);
+	return (0);
 }
